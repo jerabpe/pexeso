babaaebdfa6fa1efed51d271709867c015f38dd3
jerabpe5 <jerabpe5@fel.cvut.cz>
2022-05-12 15:38:41 +0200

    scoreboard functional

--- game.js
+++ game.js
@@ -15,6 +15,14 @@ function generateEmojis(size){
     return arr;
 }
 
+function time2Str(time){
+    if(time > 60){
+        return `${Math.floor(time/60)} min ${time%60} s`;
+    } else {
+        return `${time} s`;
+    }
+}
+
 class Game {
     constructor(size){
         this.counterEl = document.querySelector("#gameTime");
@@ -31,9 +39,36 @@ class Game {
         this.tiles = null;
         this.winEl = document.querySelector('#win');
         document.querySelector('#playAgainButton').addEventListener('click', e => this.createGame());
+        document.querySelector('#winScoreBoardButton').addEventListener('click', e => {
+            this.hide();
+            scoreboard.show();
+        })
         this.wonTextEl = document.querySelector('#wonText');
         this.nameInput = document.querySelector('#name');
         this.gameEl = document.querySelector('#game');
+        this.saveScoreButton = document.querySelector('#saveScoreButton');
+        this.scoreForm = document.querySelector('#scoreForm');
+        this.saveScoreButton.addEventListener('click', e => {
+            this.saveScore();
+            this.hideNameInput();
+        });
+        this.nameInput.addEventListener('keyup', e => {
+            if(e.key == 'Enter'){
+                e.preventDefault();
+                this.saveScore();
+                this.hideNameInput();
+            }
+        });
+    }
+
+    saveScore(){
+        const nameInput = this.nameInput.value;
+        const score = new Score(nameInput, this.timer, this.moveCounter);
+        window.localStorage.setItem(JSON.stringify(score), '');
+    }
+
+    hideNameInput(){
+        this.scoreForm.style.visibility = 'hidden';
     }
 
     clearGame(){
@@ -143,27 +178,26 @@ class Game {
         this.gameStarted = false;
         clearInterval(this.timerLoop);
         this.found = 0;
-        if(this.tiles){
-            const audio = new Audio();
-            audio.src = 'assets/success-fanfare-trumpets.mp3';
-            audio.play();
-            setTimeout(() => {
-                for(const t of this.tiles){
-                    t.style.visibility = 'visible';
-                    t.classList.remove('flipped');
-                }
-            }, 1100);
-            setTimeout(() => {
-                for(const t of this.tiles){
-                    t.classList.add('flipped');
-                }
-                this.winEl.style.visibility = 'visible';
-                const timeStr = this.timer >= 60 ? `${Math.floor(game.timer/60)} min ${game.timer%60} s`: `${this.timer} s`;
-                this.wonTextEl.innerText = `You won with ${this.moveCounter} moves in ${timeStr}`;
-                this.board.style.filter = 'blur(2px)';
-                this.nameInput.focus();
-            }, 1500);
-        }
+        const audio = new Audio();
+        audio.src = 'assets/success-fanfare-trumpets.mp3';
+        audio.play();
+        setTimeout(() => {
+            for(const t of this.tiles){
+                t.style.visibility = 'visible';
+                t.classList.remove('flipped');
+            }
+        }, 1100);
+        setTimeout(() => {
+            for(const t of this.tiles){
+                t.classList.add('flipped');
+            }
+            this.winEl.style.visibility = 'visible';
+            this.scoreForm.style.visibility = 'visible';
+            const timeStr = this.timer >= 60 ? `${Math.floor(game.timer/60)} min ${game.timer%60} s`: `${this.timer} s`;
+            this.wonTextEl.innerText = `You won with ${this.moveCounter} moves in ${timeStr}`;
+            this.board.style.filter = 'blur(2px)';
+            this.nameInput.focus();
+        }, 1500);
     }
 
     incrementTimer(game){
@@ -186,31 +220,44 @@ class Score {
         this.time = time;
         this.moves = moves;
     }
-
-    time2Str(){
-        if(time > 60){
-            return `time: ${Math.floor(time/60)} min ${time%60} s`;
-        } else {
-            return `time: ${time} s`;
-        }
-    }
 }
 
 class Scoreboard {
     constructor(){
-        //todo
         this.table = document.querySelector('#scoreTable');
         this.scoreboardEl = document.querySelector('#scoreboard');
+        this.storage = window.localStorage;
     }
 
     show(){
         this.scoreboardEl.style.display = 'flex';
+        const keys = Object.keys(window.localStorage);
+        const scores = [];
+        for(let s of keys){
+            scores.push(JSON.parse(s));
+
+        }
+        scores.sort((a,b) => {
+            if(a.moveCounter == b.moveCounter){
+                return a.timer - b.timer;
+            } else {
+                return a.moveCounter - b.moveCounter;
+            }
+        });
         for(let i=0; i<10; i++){
             const tr = this.table.insertRow();
+            let name = '';
+            let moves = '';
+            let time = '';
+            if(i<scores.length){
+                name = scores[i].name;
+                moves = scores[i].moves;
+                time = time2Str(scores[i].time);
+            }
             tr.innerHTML = `<td>${i+1}.</td>
-                            <td>john</td>
-                            <td></td>
-                            <td></td>`;
+                            <td>${name}</td>
+                            <td>${moves}</td>
+                            <td>${time}</td>`;
             if(i == 0){
                 tr.classList.add('first');
             }
@@ -228,8 +275,8 @@ class Scoreboard {
     }
 }
 
-const game = new Game(2);
 const scoreboard = new Scoreboard();
+const game = new Game(2);
 game.createGame();
 
 const newGameButton = document.querySelector("#newGameButton");
